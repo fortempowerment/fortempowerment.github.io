2c8e5636f221942a67216fdae5a2a5505687c931
blurpshell <blurpshell@protonmail.com>
2018-04-03 20:04:34 +0100

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+	$(function(){
+    var includes = $('[data-include]');
+    jQuery.each(includes, function(){
+      var file = '/../includes/' + $(this).data('include') + '.html';
+      $(this).load(file);
+    });
+  });
+
+});
